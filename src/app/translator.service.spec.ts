b0dfe5830ace580eae9a5156ee5e5b0cb223dc96
radnotiz <radnotiz@users.noreply.github.com>
2018-03-31 22:33:11 +0200

    added test for whitespaces

--- src/app/translator.service.spec.ts
+++ src/app/translator.service.spec.ts
@@ -13,10 +13,16 @@ describe('TranslatorService', () => {
     expect(service).toBeTruthy();
   }));
 
+  it('should not translate whitespaces', inject([TranslatorService], (service:TranslatorService) => {
+    expect(service.toKotkot(' al  ma   ')).toEqual([
+      'kot-kot-kodács-kodács-kodács-kodács-kot kot-kot-kodács-kot-kot-kodács-kodács',
+      'kot-kot-kodács-kot-kot-kodács-kot kot-kot-kodács-kodács-kodács-kodács-kot' ]);
+  }));
+
   it('should translate ingyen soer properly', inject([TranslatorService], (service:TranslatorService) => {
     expect(service.toKotkot('ingyen soer')).toEqual([
       'kot-kot-kodács-kot-kodács-kodács-kot kot-kot-kodács-kot-kot-kot-kodács kot-kot-kodács-kodács-kot-kot-kot kot-kot-kot-kot-kodács-kodács-kot kot-kot-kodács-kodács-kot-kodács-kot kot-kot-kodács-kot-kot-kot-kodács',
       'kot-kot-kot-kodács-kodács-kot-kot kot-kot-kodács-kot-kot-kot-kot kot-kot-kodács-kodács-kot-kodács-kot kot-kot-kot-kodács-kodács-kot-kodács'
     ]);
-  }))
+  }));
 });
