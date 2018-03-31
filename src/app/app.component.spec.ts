a9e53f1308334cbad83cf4e34210cd272b3a639a
radnotiz <radnotiz@users.noreply.github.com>
2018-03-31 18:24:40 +0200

    added i18n and hu translation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,13 +27,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Kotkot');
+    expect(app.title).toEqual('KotKot');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Kotkot!');
+    expect(compiled.querySelector('h1').textContent).toContain('KotKot');
   }));
 });
